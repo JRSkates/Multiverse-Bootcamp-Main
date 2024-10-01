01577ce1104e7f5d4421fc2c60290eae3f6fb053
JRSkates <skatesproduction@gmail.com>
2024-10-01 14:15:50 +0100

    pi decimal count function created

--- Week-1/Day-2/piCount.js
+++ Week-1/Day-2/piCount.js
@@ -1,7 +1,20 @@
 const pi =
   "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196";
 
-let piNumber = Number(pi);
-console.log(piNumber);
+//let piNumber = Number(pi);
+
+// console.log(piNumber);
+// console.log(piNum.toString().length - 2);
+// console.log(piNum)
+// console.log(count)
 
 let decimalCount = 15;
+
+const isTrue = (count, piNum) => {
+    let finalPi = Number(piNum).toString().length - 2
+    if (count === finalPi) {
+        return true;
+    }
+}
+
+console.log(isTrue(decimalCount, pi));
