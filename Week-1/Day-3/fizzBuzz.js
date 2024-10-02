5bc956abeffd736c0943c805b34f41fe553516ff
JRSkates <skatesproduction@gmail.com>
2024-10-02 14:19:51 +0100

    fizzbuzz function created with random number

--- Week-1/Day-3/fizzBuzz.js
+++ Week-1/Day-3/fizzBuzz.js
@@ -1,18 +1,19 @@
-let num = 5;
-// num = 9;
-// num = 14;
-// num = 45;
+const randomFizzBuzz = () => {
+    let num = Math.floor((Math.random() * 10000) + 1);
+    let final
 
-let solution;
+    if (num % 15 == 0) {
+        final = "FIZZBUZZ"
+    } else if (num % 5 == 0) {
+        final = "BUZZ"
+    } else if (num % 3 == 0) {
+        final = "FIZZ"
+    } else {
+        final = num
+    }
 
-if (num % 15 == 0) {
-    solution = "FIZZBUZZ"
-} else if (num % 5 == 0) {
-    solution = "BUZZ"
-} else if (num % 3 == 0) {
-    solution = "FIZZ"
-} else {
-    solution = num
+    console.log(num);
+    return final;
 }
 
-console.log(solution);
+console.log(randomFizzBuzz());
