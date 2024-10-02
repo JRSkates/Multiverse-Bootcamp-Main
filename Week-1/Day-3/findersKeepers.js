e4feb88cf12277354daa89e3f1e6efa4512f6f5b
JRSkates <skatesproduction@gmail.com>
2024-10-02 11:45:33 +0100

    finders keepers challenge

---
+++ Week-1/Day-3/findersKeepers.js
@@ -0,0 +1,14 @@
+let item = "Penny";
+// let item = 'Dime';
+// let item = 'Rock';
+// let item = 'Paper';
+
+let found;
+
+if (item == "Penny" || item == "Dime") {
+    found = true
+} else {
+    found = false
+}
+
+console.log(found);
