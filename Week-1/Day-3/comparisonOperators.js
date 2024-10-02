8acd1a1b1755b7498899e3d5960374e3736c6873
JRSkates <skatesproduction@gmail.com>
2024-10-02 11:28:46 +0100

    comparison operators

---
+++ Week-1/Day-3/comparisonOperators.js
@@ -0,0 +1,11 @@
+const submittedUserId = "485912";
+
+const storedUserId = 485912;
+
+const hasAccess = storedUserId === Number(submittedUserId);
+
+// if(storedUserId === Number(submittedUserId)) {
+//     hasAccess = true
+// } else {
+//     hasAccess = false
+// }
