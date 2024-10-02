3bf9dfb8c95cd9240f9e03af84790e5e1a9ff798
JRSkates <skatesproduction@gmail.com>
2024-10-02 14:52:04 +0100

    formatting

--- Week-1/Day-3/fizzBuzz.js
+++ Week-1/Day-3/fizzBuzz.js
@@ -1,15 +1,15 @@
 const randomFizzBuzz = () => {
     let num = Math.floor((Math.random() * 10000) + 1);
-    let final
+    let final;
 
     if (num % 15 == 0) {
-        final = "FIZZBUZZ"
+        final = "FIZZBUZZ";
     } else if (num % 5 == 0) {
-        final = "BUZZ"
+        final = "BUZZ";
     } else if (num % 3 == 0) {
-        final = "FIZZ"
+        final = "FIZZ";
     } else {
-        final = num
+        final = num;
     }
 
     console.log(num);
