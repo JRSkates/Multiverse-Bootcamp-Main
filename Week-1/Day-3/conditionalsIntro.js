c2d9ea45f17669698bcf60e9ee51c59226720995
JRSkates <skatesproduction@gmail.com>
2024-10-02 11:24:49 +0100

    conditionals intro

---
+++ Week-1/Day-3/conditionalsIntro.js
@@ -0,0 +1,17 @@
+let question1 = 92 > 32;
+let question2 = 32 === "32" || 100 > 121;
+let question3 = "happy";
+let answer1;
+let answer2;
+let answer3;
+
+answer1 = true;
+answer2 = false;
+
+if(question3 == "happy") {
+    answer3 = "I am happy"
+} else if (question3 = "sad") {
+    answer3 = "I am sad"
+} else {
+    answer3 = "I am neither happy nor sad"
+}
