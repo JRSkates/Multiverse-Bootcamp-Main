98e8cf1966965c8df8ed7b4995bc0e4eba89b8dd
JRSkates <skatesproduction@gmail.com>
2024-10-03 15:16:20 +0100

    dog age calculator

---
+++ Week-1/Day-4/dogAgeCalculator.js
@@ -0,0 +1,5 @@
+const calculateDogAge = (age) => {
+    return `Your Dog is ${age * 7} years old in Dog Years!`
+}
+
+console.log(calculateDogAge(3)); // Output: Your Dog is 21 years old in Dog Years!
