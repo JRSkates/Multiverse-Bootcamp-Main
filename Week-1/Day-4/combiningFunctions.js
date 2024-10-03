5ba89b165f77dc727f99e7f64be2cacc660ad95f
JRSkates <skatesproduction@gmail.com>
2024-10-03 14:13:44 +0100

    formatting

--- Week-1/Day-4/combiningFunctions.js
+++ Week-1/Day-4/combiningFunctions.js
@@ -1,46 +1,45 @@
 function plus(a, b) {
-    let result = a + b;
-    return result;
-  }
-  let sum = plus(2, 3);
-  console.log("Sum: ", sum);
-  
-  function multiply(a, b) {
-    let result = a * b;
-    return result;
-  }
-  
-  const product = multiply(2, 3);
-  console.log("Product: ", product);
-  
-  
-  const division = (a, b) => {
-      let result = a / b;
-      return result;
-  }
-  
-  // Using one function output as a parameter for another function
-  console.log(
-    "An example of one function using the output of another:",
-    multiply(sum, plus(1, 2))
-  );
-  
-  function isEven(x) {
-    if (x % 2 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
+  let result = a + b;
+  return result;
+}
 
-  let num = 10;
-  if (isEven(10)) {
-    console.log(num + " is Even!");
+let sum = plus(2, 3);
+console.log("Sum: ", sum);
+
+function multiply(a, b) {
+  let result = a * b;
+  return result;
+}
+
+const product = multiply(2, 3);
+console.log("Product: ", product);
+
+
+const division = (a, b) => {
+    let result = a / b;
+    return result;
+}
+
+// Using one function output as a parameter for another function
+console.log(
+  "An example of one function using the output of another:",
+  multiply(sum, plus(1, 2))
+);
+
+function isEven(x) {
+  if (x % 2 === 0) {
+    return true;
   } else {
-    console.log(num + " is not Even!");
+    return false;
   }
+}
 
-  console.log("Is 10 even?", isEven(10));
-  console.log("Is 9 even?", isEven(9));
+let num = 10;
+if (isEven(10)) {
+  console.log(num + " is Even!");
+} else {
+  console.log(num + " is not Even!");
+}
+console.log("Is 10 even?", isEven(10));
+console.log("Is 9 even?", isEven(9));
   
