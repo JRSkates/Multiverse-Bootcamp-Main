0893fe7d90d31d752e75132b5c9a6504bb5afe13
JRSkates <skatesproduction@gmail.com>
2024-10-04 16:55:30 +0100

    sum of chars function created

---
+++ Week-1/Day-5/sumOfChars.js
@@ -0,0 +1,6 @@
+const sumOfChars = (a, b) => {
+    return a.length + b.length;
+}
+
+console.log(sumOfChars("Hello", "World")); // Output: 10
+console.log(sumOfChars("Greetings", "Universe")); // Output: 17
