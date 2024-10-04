175fe2231c5eaf2164cdcb7703ccb72af500ac28
JRSkates <skatesproduction@gmail.com>
2024-10-04 13:36:03 +0100

    formatting and console logging player choices

--- Week-1/Day-5/rockPaperScissors.js
+++ Week-1/Day-5/rockPaperScissors.js
@@ -11,6 +11,8 @@ const randomChoice = () => {
 }
 
 const winner = (player1, player2) => {
+    console.log(`Player 1 chose: ${player1}`);
+    console.log(`Player 2 chose: ${player2}`);
     if (player1 == player2) {
         return "It's a tie";
     } else if ((player1 == "paper" && player2 == "rock") || (player1 == "rock" && player2 == "scissors") || (player1 == "scissors" && player2 == "paper")) {
