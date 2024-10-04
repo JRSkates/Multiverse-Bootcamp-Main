fc6583751b2bdc655983eabce304651032f40984
JRSkates <skatesproduction@gmail.com>
2024-10-04 13:31:54 +0100

    rock paper scissors completed

--- Week-1/Day-5/rockPaperScissors.js
+++ Week-1/Day-5/rockPaperScissors.js
@@ -1,11 +1,23 @@
 const randomChoice = () => {
     const choice = Math.floor(Math.random() * 3);
-   // console.log(choice)
+
     if (choice == 0) {
         return "rock";
     } else if (choice == 1) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 }
+
+const winner = (player1, player2) => {
+    if (player1 == player2) {
+        return "It's a tie";
+    } else if ((player1 == "paper" && player2 == "rock") || (player1 == "rock" && player2 == "scissors") || (player1 == "scissors" && player2 == "paper")) {
+        return "Player 1 Wins!";
+    } else {
+        return "Player 2 Wins!";
+    }
+}
+
+console.log(winner(randomChoice(), randomChoice()));
