823f02339c65541174b9f5e4f006f0d80442e71a
JRSkates <skatesproduction@gmail.com>
2024-10-07 11:57:35 +0100

    avengers array

---
+++ Week-2/Day-1/avengers.js
@@ -0,0 +1,14 @@
+let thirdAvenger;
+
+const avengers = [
+    "Iron Man", 
+    "Thor", 
+    "Captain America", 
+    "The Hulk", 
+    "Spider Man", 
+    "Ant Man", 
+    "Batman"
+]
+
+thirdAvenger = avengers[2];
+avengers[3] = "Captain Marvel";
