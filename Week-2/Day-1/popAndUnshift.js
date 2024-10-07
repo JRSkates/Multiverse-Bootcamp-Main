67b8a956a597d279ea50c8adbc5f82cf489893ef
JRSkates <skatesproduction@gmail.com>
2024-10-07 11:10:29 +0100

    push and unshift methods

---
+++ Week-2/Day-1/popAndUnshift.js
@@ -0,0 +1,14 @@
+const travelDestinations = [
+    "Paris",
+    "Tokyo",
+    "Bali",
+    "New York City",
+    "Cape Town",
+];
+
+let arrLen;
+
+travelDestinations.push("Cairo");
+travelDestinations.unshift("Mexico City")
+
+arrLen = 7;
