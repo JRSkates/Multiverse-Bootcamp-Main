804a8d2a42a1852d314439f51b524ac63f8383aa
JRSkates <skatesproduction@gmail.com>
2024-10-08 14:43:47 +0100

    object properties

---
+++ Week-2/Day-2/objectProperties.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Angelica Daniels",
+  age: 35,
+  profession: "Web Developer",
+  interests: ["coding", "reading", "playing guitar"],
+};
+
+let currentRole;
+
+currentRole = person.profession;
+person.location = "London";
+  
