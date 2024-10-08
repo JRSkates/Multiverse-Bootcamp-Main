03cd0f894d700bd7c4185e1d126d48c4b14c8b46
JRSkates <skatesproduction@gmail.com>
2024-10-08 14:54:49 +0100

    car object

---
+++ Week-2/Day-2/carObject.js
@@ -0,0 +1,13 @@
+const car = {
+    model: "Nissan",
+    color: "Blue",
+    numWheels: 4,
+    drive() {
+        return "Vroom!"
+    }
+}
+
+console.log(car.model) // output: Nissan
+console.log(car.color) // output: Blue
+console.log(car.numWheels) // output: 4
+console.log(car.drive()) // output: Vroom!
