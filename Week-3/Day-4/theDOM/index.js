e5fe5b97b3f1321c4611146a94b5f7c35b5f261b
JRSkates <skatesproduction@gmail.com>
2024-10-17 11:18:07 +0100

    the DOM and family/children tree

---
+++ Week-3/Day-4/theDOM/index.js
@@ -0,0 +1,13 @@
+let h1;
+let container;
+let flexChild2;
+
+h1 = document.querySelector('h1')
+container = document.querySelector(".flexbox")
+flexChild2 = document.querySelector("#item2")
+
+module.exports = {
+  h1,
+  container,
+  flexChild2,
+};
