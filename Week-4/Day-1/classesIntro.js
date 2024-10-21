dc408b36f7138a8a888e8ad5db1d1bb73ca9067b
JRSkates <skatesproduction@gmail.com>
2024-10-21 12:02:19 +0100

    classes intro

---
+++ Week-4/Day-1/classesIntro.js
@@ -0,0 +1,11 @@
+class Bag {
+    constructor(name, weight, id) {
+        this.name = name
+        this.weight = weight
+        this.id = id
+    }
+}
+
+const bagOne = new Bag("Main Bag", 68, 1)
+
+console.log(bagOne)
