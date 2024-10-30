43e1a6d9bc70d0fe26745bb2f6df0225348c7317
JRSkates <skatesproduction@gmail.com>
2024-10-30 15:00:12 +0000

    User model class reformatted with a static initializer

--- Week-5/Day-3/src/models/User.js
+++ Week-5/Day-3/src/models/User.js
@@ -1,18 +1,21 @@
 const { db, DataTypes, Model } = require('../../db/connection');
 
 // extend the Sequlize builder Model
-class User extends Model {}
-
-// structure the table with datatypes
-// also sequlize connection and model name
-User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-}, {
-    sequelize: db,
-    modelName: 'User',
-})
+class User extends Model {
+    // static initializer
+    static {
+        // structure the table with datatypes
+        // also sequlize connection and model name
+        User.init({
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING
+        }, {
+            sequelize: db,
+            modelName: 'User',
+        })
+    }
+}
 
 module.exports = User;
 // this runs to create the database
