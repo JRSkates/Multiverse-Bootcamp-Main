9b68d659b88b079fcec18e17bf020698ce64a1bf
JRSkates <skatesproduction@gmail.com>
2024-10-30 12:55:03 +0000

    require fixed

--- Week-5/Day-3/db/connection.js
+++ Week-5/Day-3/db/connection.js
@@ -1,11 +1,9 @@
-const Sequelize = require('sequelize');
+const { DataTypes, Model, Sequelize} = require('sequelize');
 const path = require('path');
 
 const db = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, 'db.sqlite')
 });
-
-const { DataTypes, Model } = Sequelize;
 
 module.exports = { db, DataTypes, Model };
