2cd5019b95ebb3cbb396ef64a32586cf6d6f16ae
JRSkates <skatesproduction@gmail.com>
2024-10-30 12:53:23 +0000

    define a table model with Sequlize

---
+++ Week-5/Day-3/models/User.js
@@ -0,0 +1,20 @@
+const { db, DataTypes, Model } = require('../db/connection');
+
+// extend the Sequlize builder Model
+class User extends Model {}
+
+// structure the table with datatypes
+// also sequlize connection and model name
+User.init({
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+}, {
+    sequelize: db,
+    modelName: 'User',
+})
+
+// this runs to create the database
+db.sync();
+
+// this defines a table inside our database
