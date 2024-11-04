8e1697a8e06564ca619ae5b3cb8b4d8b4a5da3f8
JRSkates <skatesproduction@gmail.com>
2024-11-04 12:36:59 +0000

    application and random route created with .get

--- Week-6/Day-1/src/app.js
+++ Week-6/Day-1/src/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 
+// run with: node server.js
 app.use("/myHTML", express.static("public"))
 
+// localhost:3000/application
+app.get("/application", (req, res) => {
+    const hello = "Hello, World!";
+    res.send(hello);
+})
+
+//localhost:3000/random
+app.get("/random", (req, res) => {
+    const random = Math.floor(Math.random() * 3)
+    res.send(`Random number: ${random}`)
+  });
 
 module.exports = app;
