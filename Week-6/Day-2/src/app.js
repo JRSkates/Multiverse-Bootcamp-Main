74d4860af3a34b5247d8252e9e53909d34b07ce5
JRSkates <skatesproduction@gmail.com>
2024-11-05 12:07:10 +0000

    Express CRUD with GET, POST, PUT and DELETE routes

--- Week-6/Day-2/src/app.js
+++ Week-6/Day-2/src/app.js
@@ -2,9 +2,30 @@ const express = require('express');
 const app = express();
 const fruits = ["apple", "orange", "banana"]
 
-app.get('/rest', (req, res) => {
+app.use(express.json());
+
+//CREAT - Post
+app.post('/', (req, res) => {
+    fruits.push(req.body.fruit);
+    res.json({fruits});
+})
+
+//READ - Get
+app.get('/', (req, res) => {
     res.json({fruits})
 })
 
+//UPDATE - Push/Patch
+app.put('/', (req, res) => {
+    fruits[0] = req.body.fruit;
+    res.json({fruits});
+});
+
+
+//DELETE - Delete
+app.delete('/', (req, res) => {
+    fruits.pop();
+    res.json({fruits});
+});
 
 module.exports = app;
