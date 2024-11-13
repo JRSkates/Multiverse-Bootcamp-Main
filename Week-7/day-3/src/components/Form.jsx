0f17efcc4658b8165f4462379cf6c779be01a35d
JRSkates <skatesproduction@gmail.com>
2024-11-13 14:12:06 +0000

    submit handlers wiwith array state

--- Week-7/day-3/src/components/Form.jsx
+++ Week-7/day-3/src/components/Form.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("")
+  const [name, setName] = useState("")
   const [age, setAge] = useState(20)
+  const [namesArr, setNamesArr] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // Prevents the page from refreshing
+    console.log(`Submitting form with first name: ${name} and age: ${age}`)
+    setNamesArr([...namesArr, {name: name, age: age}])
+    setName("")
+    setAge(20)
+  }
   
   function handleNameChange (e) {
-      setFirstName(e.target.value)
+      setName(e.target.value)
   }
 
   function handleAgeChange (e) {
@@ -14,21 +24,37 @@ function Form() {
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           First name:
           {/* First name input element */}
-          <input value={firstName} onChange={handleNameChange}/>
+          <input 
+            value={name} 
+            onChange={handleNameChange}
+          />
         </label>
 
         {/* Age input element */}
         <label>
           Age:
-          <input type="number" value={age} onChange={handleAgeChange}/>
+          <input 
+            type="number" 
+            value={age} 
+            onChange={handleAgeChange}
+          />
         </label>
+        <button>
+            <span className='material-icons'>send</span>
+        </button>
       </form>
-      <p className="name">Your name is {firstName}</p>
+      <p className="name">Your name is {name}</p>
       <p className="age">Your age is {age}</p>
+      <div id="card">
+        <h3>Names & Ages Submitted</h3>
+        {namesArr.map((item) => (
+          <p>{item.name} - {item.age}</p>
+        ))}
+      </div>
     </>
   );
 }
