fe94a9f6624b5c9a20bc6442a3ddb2ddeadd078b
JRSkates <skatesproduction@gmail.com>
2024-11-14 14:03:57 +0000

    Use effect to call all in the array from the API

--- Week-7/Day-4/day-4-react/src/App.jsx
+++ Week-7/Day-4/day-4-react/src/App.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("")
+  const [pokeArr, setPokeArr] = useState([])
+
+  const pokeRequest = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=15");
+    const data = await res.json();
+    // Update state
+    setPokeArr(data.results)
+  };
+
 
   async function handleClick() {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=15')
@@ -9,11 +18,19 @@ function App() {
       console.log(data.results[0].name)
       setPokemonName(data.results[0].name)
   }
+
+  useEffect(() => {
+    pokeRequest();
+  }, [])
   
   return (
     <div className="poke-div" pokemon-name={pokemonName}>
       <div id="pokemon-name">{pokemonName}</div>
       <button onClick={handleClick}>Show Me the Pokemon!</button>
+      <h1>Gotta Catch Em All</h1>
+      {pokeArr.map((poke) => (
+        <p>{poke.name}</p>
+      ))}
     </div>
   );
 }
